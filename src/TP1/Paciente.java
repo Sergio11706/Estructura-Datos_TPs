978db9df3617e59bee2c78b7b86511e47ba25b94
Daniel Ramos <daniel.rsi6oa@gmail.com>
2026-08-23 16:35:31 -0300

    Ejercicio 4

--- src/TP1/Paciente.java
+++ src/TP1/Paciente.java
@@ -1,15 +1,40 @@
 package TP1;
 
 public class Paciente {
-    String nombre;
-    int dni;
-    double peso;
-    double altura;
+    private String nombre;
+    private int dni;
+    private double peso;
+    private double altura;
 
-    public Paciente(String nombre, int dni, double peso, double altura){ 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
         this.dni = dni;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
         this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
         this.altura = altura;
     }
 
