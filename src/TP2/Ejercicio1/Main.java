dcd81bdd15b373e6dc5dc1e2ec26beaaefc774c4
Sergio Acuña <sergioacu17@gmail.com>
2026-08-29 22:53:55 -0300

    Agregando comentarios

--- src/TP2/Ejercicio1/Main.java
+++ src/TP2/Ejercicio1/Main.java
@@ -3,12 +3,38 @@ package TP2.Ejercicio1;
 import java.util.Scanner;
 import java.util.random.RandomGenerator;
 
+/**
+ * Clase principal para el ejercicio 1 del TP2.
+ * 
+ * @author Sergio Acuña
+ * 
+ * Enunciado:
+ *    Desarrollar un programa que solicite al usuario un valor entero N.
+ *    - Generar N números aleatorios (entre -50 y 50) y cargarlos en un arreglo.
+ *    - Luego el programa debe implementar:
+ * 
+ *      a) Un método que reciba el arreglo inicial y devuelva otro arreglo
+ *         que contenga los elementos desplazados una posición hacia la izquierda
+ *         (el primer elemento debe pasar a la última posición).
+ * 
+ *      b) Un método que calcule y muestre la suma de los números positivos
+ *         y la suma de los números negativos por separado.
+ * 
+ *      c) Un método que invierta el orden de los elementos del arreglo original
+ *         modificándolo directamente (sin crear un arreglo nuevo) y luego lo muestre.
+ */
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("\nIngrese la cantidad de números a generar: ");
-        int cantidadNumeros = sc.nextInt();
+        // Solicitar un numero hasta que sea mayor a 0
+        int cantidadNumeros = 0;
+        do{
+            System.out.print("\nIngrese la cantidad de números a generar: ");
+            cantidadNumeros = sc.nextInt();
+        } while(cantidadNumeros<=0);
 
         System.err.println("\n**Arreglo generado**");
         int[] numeros = new int[cantidadNumeros];
@@ -41,13 +67,15 @@ public class Main {
 
     private static int[] arregloDesplazado(int[] numeros) {
         int[] arregloDesplazado = new int[numeros.length];
+        
         for (int i = 0; i < numeros.length; i++) {
-            if (i == numeros.length - 1) {
+            if (i == numeros.length - 1) { //Si es el ultimo elemento debe pasar a la primera posicion
                 arregloDesplazado[i] = numeros[0];
             } else {
                 arregloDesplazado[i] = numeros[i + 1];
             }
         }
+
         return arregloDesplazado;
     }
 
@@ -73,7 +101,7 @@ public class Main {
     private static void invertirArreglo(int[] numeros){
         int aux=0, j=numeros.length-1;
         for(int i=0; i<numeros.length/2; i++){
-            aux=numeros[j];
+            aux=numeros[j]; // El auxiliar guarda el valor para no perderlo
             numeros[j]=numeros[i];
             numeros[i]=aux;
             j--;
