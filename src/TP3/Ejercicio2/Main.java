66ba5d8aabb71e6e365d63a5b138478d5e8a969a
Sergio Acuña <sergioacu17@gmail.com>
2026-09-05 12:15:11 -0300

    Agregando carpeta TP2

---
+++ src/TP3/Ejercicio2/Main.java
@@ -0,0 +1,17 @@
+package TP3.Ejercicio2;
+
+import TP3.common.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Integer> pila = new Stack<>();
+        
+        pila.push(10);
+        pila.push(20);
+        pila.push(30);
+
+        System.out.println("Hola Andreu"); 
+
+    }
+}
